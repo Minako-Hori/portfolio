02d607bb4b3ca266aee7e98aa612629d3a5c8834
m.hori <ame29hakka@gmail.com>
2020-01-08 05:10:52 +0900

    画像の追加と、桜散らすのなしに変更しました。

--- js/index.js
+++ js/index.js
@@ -39,11 +39,11 @@ $(function () {
     });
 });
 
-$(document).ready(function () {
-    $('.background').snowfall({
-        image: "image/sakura_hanabira3.png",
-        maxSize: 30,
-        flakeIndex: "888",
-        maxSpeed: 3
-    });
-});
+// $(document).ready(function () {
+    // $('.background').snowfall({
+        // image: "image/sakura_hanabira3.png",
+        // maxSize: 30,
+        // flakeIndex: "888",
+        // maxSpeed: 3
+    // });
+// });
